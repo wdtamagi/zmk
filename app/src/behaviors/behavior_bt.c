0119f0b9840584a4a2d31bfa6b77fdd6569b9cf2
Peter Johanson <peter@peterjohanson.com>
2024-03-27 12:27:49 -0700

    feat(behaviors): Add behavior metadata information.
    * For upcoming ZMK studio work, make a set of rich metadata available
      to provide a friendly name for a behavior, and allow super flexible
      descriptions of the parameters the behaviors take.

--- app/src/behaviors/behavior_bt.c
+++ app/src/behaviors/behavior_bt.c
@@ -20,6 +20,73 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static const struct behavior_parameter_value_metadata no_arg_values[] = {
+    {
+        .position = 0,
+        .friendly_name = "Next Profile",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_VALUE,
+        .value = BT_NXT_CMD,
+    },
+    {
+        .position = 0,
+        .friendly_name = "Previous Profile",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_VALUE,
+        .value = BT_PRV_CMD,
+    },
+    {
+        .position = 0,
+        .friendly_name = "Clear All Profiles",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_VALUE,
+        .value = BT_CLR_ALL_CMD,
+    },
+    {
+        .position = 0,
+        .friendly_name = "Clear Selected Profile",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_VALUE,
+        .value = BT_CLR_CMD,
+    },
+};
+
+static const struct behavior_parameter_metadata_custom_set no_args_set = {
+    .values = no_arg_values,
+    .values_len = ARRAY_SIZE(no_arg_values),
+};
+
+static const struct behavior_parameter_value_metadata prof_index_values[] = {
+    {
+        .position = 0,
+        .friendly_name = "Select Profile",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_VALUE,
+        .value = BT_SEL_CMD,
+    },
+    {
+        .position = 0,
+        .friendly_name = "Disconnect Profile",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_VALUE,
+        .value = BT_DISC_CMD,
+    },
+    {
+        .position = 1,
+        .friendly_name = "Profile",
+        .type = BEHAVIOR_PARAMETER_VALUE_METADATA_TYPE_RANGE,
+        .range = {.min = 0, .max = ZMK_BLE_PROFILE_COUNT},
+    },
+};
+
+static const struct behavior_parameter_metadata_custom_set profile_index_metadata_set = {
+    .values = prof_index_values,
+    .values_len = ARRAY_SIZE(prof_index_values),
+};
+
+static const struct behavior_parameter_metadata_custom metadata = {
+    .sets_len = 2,
+    .sets = {no_args_set, profile_index_metadata_set},
+};
+
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     switch (binding->param1) {
@@ -54,6 +121,9 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 static const struct behavior_driver_api behavior_bt_driver_api = {
     .binding_pressed = on_keymap_binding_pressed,
     .binding_released = on_keymap_binding_released,
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+    .custom_parameters = &metadata,
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
 };
 
 BEHAVIOR_DT_INST_DEFINE(0, behavior_bt_init, NULL, NULL, NULL, POST_KERNEL,
